70746bbe0c17edc5da01885dd472fc01655c8f4c
ashishsorte123 <ashishsorte983@gmail.com>
2021-08-08 10:18:57 +0530

    'initial'

--- server.js
+++ server.js
@@ -1,57 +1,39 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const path = require('path');
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 const cors = require('cors');
 
-app.use(express.static('public'));
-app.use(express.json());
-
-require('dotenv').config();
-
-const mongoose = require('mongoose');
-
-function connectDB() {
-    // Database connection
-
-    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, 
-    useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true }); 
-    const connection = mongoose.connection;
-
-    connection.once('open', () => {
-        console.log('Database connected.');
-    }).catch(err => {
-        console.log('Connection Failed.');
-    })
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
 
-module.exports = connectDB;
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
 
-// const connectDB = require('./config/db');
-// connectDB.connectDB();
+app.use(cors(corsOptions));
+app.use(express.static('public'));
+
+const connectDB = require('./config/db');
 connectDB();
 
-// Cors
-const corsOption = {
-    origin: process.env.ALLOWED_CLIENTS.split(',')
-    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-
-app.use(cors(corsOption));
-
-// Template engine
+app.use(express.json());
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-// Routes
-
+// Routes 
 app.use('/api/files', require('./routes/files'));
-
 app.use('/files', require('./routes/show'));
-
 app.use('/files/download', require('./routes/download'));
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-})
+
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
